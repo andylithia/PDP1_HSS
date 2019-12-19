2631d598a711fcf4488324e9467364e5230e1033
andylithia <thelithcore@gmail.com>
2019-12-19 19:36:44 +0800

    Minor

--- pdp_core.c
+++ pdp_core.c
@@ -1,5 +1,6 @@
 #include "pdp_core.h"
 #include "PDP_ISA.h"
+#include "BCG.h"
 // Main 4K 18bit memory
 // Requires a MCU that has more than 12KBytes of RAM
 // ii[5:0] = [5:0] {INDIRECT, INSTRUCTION[4:0]}
@@ -23,12 +24,22 @@ uint32_t msecs = 0;
 
 // Global settings
 // | 7 | 6 | 5 | 4 | 3 | 2 | 1 : 0 |
-//           |   |   |   |   '------- 00: RUN, 01: HALT, 10: SST
-//           |   |   |   '----------- Enable Mul, Div
-//           |   |   '--------------- Merge R(Control) and R(Testword)
-//           |   '------------------- Odd Frame Fix
-//           '----------------------- Rotate Controls
-uint8_t settings = 0; 
+//   |   |   |   |   |   |   '------- 00: RUN, x1: HALT, 10: SST
+//   |   |   |   |   |   '----------- Enable Mul, Div
+//   |   |   |   |   '--------------- Merge R(Control) and R(Testword)
+//   |   |   |   '------------------- Odd Frame Fix
+//   |   |   '----------------------- Rotate Controls
+//   |   '--------------------------- Enhanced CRT Function
+//   '------------------------------- CRT Selectable Origin
+uint16_t settings = 0; 
+
+#define S_HWMULDIV_B    2
+#define S_MERGECTRL_B   3
+#define S_ODDFRAMEFIX_B 4
+#define S_ROTATECTRL_B  5
+#define S_ECRT_B        6
+#define S_CRTSSORIG_B   7
+
 void (*trapPtr)(void* arg);	// Trap Callback Function Pointer
 void (*plotCRTPtr)(uint16_t x, uint16_t y, uint16_t i);	// CRT Callback Pointer
 
@@ -148,11 +159,14 @@ void PDP_setCRTCallback(void(*cb)(uint16_t, uint16_t, uint16_t)){plotCRTPtr=cb;}
 			saves space for embedded platforms
 		* Shifting uses the older algorithm to save RAM space
 */
-void PDP_dispatch(uint32_t md) {
+void PDP_dispatch(uint32_t md, uint32_t xpc) {
+	uint32_t mb, t, i;
+
 	y = md&07777;
 	ib = (md>>12)&1;
+	ctc+=5;
 	// IND Check (ea)
-	switch(md>>13) {
+	switch((md>>12)&076) {
 		case SKP:
 		case SFT:
 		case LAW:
@@ -167,29 +181,54 @@ void PDP_dispatch(uint32_t md) {
 				y=memory[y]&07777;
 			};
 	}
-	switch(md>>13) {
+	switch((md>>12)&076) {
 		case AND: ac&=memory[y]; break;
 		case IOR: ac|=memory[y]; break;
 		case XOR: ac^=memory[y]; break;
-		case XCT: dispatch(memory[y]); break;
+		case XCT: dispatch(memory[y],y); break;
+		case CAL_JDA:
+			uint32_t target=(ib==0)?0100:y;
+			memory[target]=ac;
+			ac=(ov<<17)+pc;
+			pc=target+1;
+			ctc+=5;
+			break;
 		case LAC: ac=memory[y]; break;
 		case LIO: io=memory[y]; break;
 		case DAC: memory[y]=ac; break;
-		case DAP: memory[y]=(memory[y]&0770000)+(ac&07777); break;
+		case DAP: memory[y]=(memory[y]&0770000)|(ac&0007777); break;
+		case DIP: memory[y]=(memory[y]&0007777)|(ac&0770000); break;
 		case DIO: memory[y]=io; break;
 		case DZM: memory[y]=0; break;
 		case ADD:
+			mb = memory[y];
+			uint32_t ov2=(ac>>17==mb>>17);
+			ac+=mb;
+			if(ac&01000000) ac=(ac+1)&0777777;
+			if(ov2&(mb>>17!=ac>>17)) ov=1;
+			if(ac==0777777) ac=0;
+			/*
 			ac = ac+memory[y];
 			ov = ac>>18;
 			ac = (ac+ov)&0777777;
 			if(ac==0777777) ac=0;
+			*/
 			break;
 		case SUB:
+			mb=memory[y];
+			ac^=0777777;
+			uint32_t ov2=(ac>>17==mb>>17);
+			ac+=mb;
+			if(ac^01000000) ac=(ac+1)&0777777;
+			if(ov2&(mb>>17!=ac>>17)) ov=1;
+			ac^=0777777;
+			/*
 			uint32_t diffsigns = ((ac>>17)^(memory[y]>>17))==1;
 			ac += memory[y]^0777777;
 			ac = (ac+(ac>>18))&0777777;
 			if(ac==0777777) ac=0;
 			if(diffsigns&&(memory[y]>>17==ac>>17)) ov=1;
+			*/
 			break;
 		case IDX:
 			ac = memory[y]+1;
@@ -200,47 +239,142 @@ void PDP_dispatch(uint32_t md) {
 			ac = memory[y]+1;
 			if(ac==0777777) ac=0;
 			memory[y] = ac;
-			if((ac&0400000)==0) pc++;
+			if((ac&0400000)==0) pc=(xpc>=0)?xpc+1:pc+1;
 			break;
 		case SAD: if(ac!=memory[y]) pc++; break;
 		case SAS: if(ac==memory[y]) pc++; break;
-		case MUS:
-			if ((io&1)==1){
-				ac=ac+memory[y];
-				ac=(ac+(ac>>18))&0777777;
-				if (ac==0777777) ac=0;
-			}
-			io=(io>>1|ac<<17)&0777777;
-			ac>>=1;
-			break;
-		case DIS:
-			uint32_t acl=ac>>17;
-			ac=(ac<<1|io>>17)&0777777;
-			io=((io<<1|acl)&0777777)^1;
-			if ((io&1)==1){
-				ac=ac+(memory[y]^0777777);
-				ac=(ac+(ac>>18))&0777777;
+		case MUS_MUL:
+			if(settings&BIT(S_HWMULDIV_B)) {	// MUL
+				uint8_t scr, smb, srm;
+				mb=memory[y];
+				io=ac;
+				if(mb&0400000) {
+					smb=1;
+					mb^=0777777;
+				} else
+					smb=0;
+				if(io&0400000) {
+					srm=1;
+					io^=0777777;
+				} else
+					srm=0;
+				ac=0;
+				scr=1;
+				while(scr<022) {
+					if(io&1) {
+						ac+=mb;
+						if(a&01000000) ac=(ac+1)&0777777;
+					}
+					io=io>>1|((ac&1)<<17);
+					ac>>=1;
+					scr++;
+				}
+				if(smb!=srm&&(ac|io!=0)) {
+					ac=ac^0777777;
+					io=io^0777777;
+				}
+				ctc+=10;
 			} else {
-				ac=ac+1+memory[y];
-				ac=(ac+(ac>>18))&0777777;
+				if ((io&1)==1){
+					ac=ac+memory[y];
+					ac=(ac+(ac>>18))&0777777;
+					if (ac==0777777) ac=0;
+				}
+				io=(io>>1|ac<<17)&0777777;
+				ac>>=1;
 			}
-			if (ac==0777777) ac=0;
 			break;
+		case DIS_DIV:
+			if(settings&BIT(S_HWMULDIV_B)) {	// DIV
+				uint8_t scr, smb, srm;
+				mb=memory[y];
+				if(mb&0400000) smb=1;
+				else {
+					mb^=0777777;
+					smb=0;
+				}
+
+				if(ac&0400000) {
+					srm=1;
+					ac^=0777777;
+					io^=0777777;
+				} else
+					srm=0;
+				scr=0;
+				while(1) {
+					ac+=mb;
+					if (ac==0777777)		ac^=0777777;
+					else if (ac&01000000)	ac=(ac+1)&0777777;
+					if (mb&0400000)			mb^=0777777;
+					if (scr==022 || (scr==0 && (ac&0400000)==0)) break;
+					scr++;
+					if ((ac&0400000)==0) mb^=0777777;
+					t=(ac>>17)^1;
+					ac=((ac<<1)|(io>>17))&0777777;
+					io=((io<<1)|t)&0777777;
+					if ((io&1)==0) ac=(ac+1)&0777777;
+				}
+				ac+=mb;
+				if (ac==0777777)		ac^=0777777;
+				else if (ac&01000000)	ac=(ac+1)&0777777;
+				if (scr!=0) {
+					pc++;
+					ac>>=1;
+				}
+				if (srm && ac!=0777777) ac^=0777777;
+				if (scr==0) {
+					if (srm) io^=0777777;
+					ctc+=2;
+				} else {
+					if (srm!=smb && io!=0) io^=0777777;
+					mb=io;
+					io=ac;
+					ac=mb;
+					ctc+=25; // average 35 us of 30-40 us
+				}
+			} else {	// DIS
+				t=(ac>>17)^1;
+				ac=(ac<<1|io>>17)&0777777;
+				io=((io<<1|t)&0777777);
+				if (t)	ac=ac^0777777+memory[y];
+				else 	ac=((ac+1)&0777777)+memory[y];
+				if(ac&01000000)	ac=(ac+1)&0777777;
+				if(t)			ac^=0777777;
+				if(ac==0777777)	ac=0;
+			}
+			break;
+			
 		case JMP: pc=y; break;
 		case JSP: ac=(ov<<17)+pc; pc=y; break;
 		case SKP:
 			uint8_t cond =
-				(((y&0100)==0100)&&(ac==0)) ||
-				(((y&0200)==0200)&&(ac>>17==0)) ||
-				(((y&0400)==0400)&&(ac>>17==1)) ||
-				(((y&01000)==01000)&&(ov==0)) ||
-				(((y&02000)==02000)&&(io>>17==0))||
-				(((y&7)!=0)&&!flag[y&7])||
-				(((y&070)!=0)&&!sense[(y&070)>>3])||
+				(((y&0100)==0100)&&(ac==0)) ||		// SZA
+				(((y&0200)==0200)&&(ac>>17==0)) ||	// SPA
+				(((y&0400)==0400)&&(ac>>17==1)) ||	// SMA
+				(((y&01000)==01000)&&(ov==0)) ||	// SZO
+				(((y&02000)==02000)&&(io>>17==0));	// SPI
+			if ((y&01000)==01000) ov=0;
+			if(cond==0&&(y&070)) {					// SZS
+				t=(y&070)>>3;
+				if(t==7) 		cond=(sense)?0:1;
+				else if(t!=0)	cond=(sense&(1<<(t-1)))?0:1;
+			}
+			if(cond==0&&(y&07)) {					// SZF
+				t=y&07;
+				if(t==7)		cond=(flags)?0:1;
+				else if(t!=0)	cond=(flags&(1<<(t-1)))?0:1;
+			}
+			if (cond^ib) {							// negate on i-bite
+				if (xpc>=0)	pc=xpc+1
+				else 		pc++;
+			}
+			/*
+				(((y&7)!=0)&&!flag[y&7])||			// szf
+				(((y&070)!=0)&&!sense[(y&070)>>3])||// szs
 				((y&070)==010);
 			if (ib==0) {if(cond) pc++;}
 				else {if(!cond) pc++;}
-			if ((y&01000)==01000) ov=0;
+			*/
 			break;
 		case SFT:
 			uint8_t nshift=0, mask=md&0777;
@@ -282,8 +416,81 @@ void PDP_dispatch(uint32_t md) {
 		      }
 		case LAW: ac=(ib==0)?y:y^0777777; break;
 		case IOT:
-			if((y&077)==007) {dpy(); break;}
-			if((y&077)==011) {io=control; break;}
+			switch(y&077) {
+				case 000:	// I/O Wait
+					if(ib&&ioc) {
+						ioc-=ctc;
+						if(ioc>ctc) ctc=ioc;
+						ioc=0;
+					}
+					break;
+				case 007:	// dpy
+					uint32_t px,py;
+					if(settings&BIT(S_ECRT_B)) {
+						if(settings&BIT(S_CRTSSORIG_B)) {
+							switch((y>>9)&03) {
+								case 0: // origin at center
+									px=(ac & 0400000)? 511-((ac>>8)^01777) : 512+(ac>>8);
+									py=(io & 0400000)? 512+((io>>8)^01777) : 511-(io>>8);
+									break;
+								case 1: // origin at bottom edge (center)
+									px=(ac & 0400000)? 511-((ac>>8)^01777) : 512+(ac>>8);
+									py=01777-((io>>8)&01777);
+									break;
+								case 2: // origin at middle left edge
+									px=(ac>>8)&01777;
+									py=(io & 0400000)? 512+((io>>8)^01777) : 511-(io>>8);
+									break;
+								case 3: // origin at lower left corner
+									px=(ac>>8)&01777;
+									py=01777-((io>>8)&01777);
+									break;
+							}
+						} else {
+							px=(ac & 0400000)? 511-((ac>>8)^01777) : 512+(ac>>8);
+							py=(io & 0400000)? 512+((io>>8)^01777) : 511-(io>>8);
+						}
+						(*plotCRTPtr)(px, py, (y>>6)&07);
+						status&=0377777;
+						if(ib) {
+							ctc+=45;
+							ioc=0;
+						} else if((md>>11)&2) {
+							ioc=50;
+						}
+					} else {
+						px = (ac+0400000)&0777777;
+						py = (io+0400000)&0777777;
+						px = x*550/0777777;
+						py = y*550/0777777;
+						(*plotCRTPtr)(px, py, 07);
+					}
+					break;
+				case 011:	// Read paper pin block
+					t=(settings&BIT(S_MERGECTRL_B))?control|testword:control;
+					if(y&100) {
+						io=(settings&BIT(S_ODDFRAMEFIX_B))?t&15:t&0740000;
+						break;
+					}
+					io|=(settings&BIT(S_ROTATECTRL_B))?((t<<4)&0777777)|(t>>14):t;
+				case 001:	// rpa
+					t=tapeReadAlpha(md&014000==0);
+					io=t;
+					ctc=0;
+				case 002:	// rpb
+					t=tapeReadBin(md&014000==0);
+					io=t;
+					ctc=0;
+				case 030:	// rrb
+					t=tapeReadBuffer();
+					io=t;
+					ctc=0;
+					break;
+				case 033:	// cks
+					io = status;
+					break;
+				default:
+			}
 			break;
 		case OPR:
 			if((y&00200)==00200) ac=0;
@@ -302,14 +509,6 @@ void PDP_dispatch(uint32_t md) {
 		default:
 		// undefined
 	}
-}
-
-void dpy() {
-	x = (ac+0400000)&0777777;
-	y = (io+0400000)&0777777;
-	x = x*550/0777777;
-	y = y*550/0777777;
-	// TODO: write DAC
 }
 
 uint8_t* tdata;
